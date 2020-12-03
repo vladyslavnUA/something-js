843a3a6276b73caa39fc8f0be30a403f7dbc837f
vladyslavnUA <vladyslavn@yahoo.com>
2020-12-02 20:42:29 -0500

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+const addPls = (a, b) => {
+    return (a + b);
+}
+
+const subtractPls = (a, b) => {
+    return (a - b);
+}
+
+const multiplyPls = (a, b) => {
+    return (a * b);
+}
+
+const dividePls = (a, b) => {
+    return (a / b);
+}
+
+module.exports = {
+    addPls,
+    subtractPls,
+    multiplyPls,
+    dividePls
+}
